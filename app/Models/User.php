f5e0823956dd89a0d82f89cb19260472806d3762
qiang3272008 <592456287@qq.com>
2019-02-03 08:00:47 +0000

    移动 user 模型到 app/models 目录

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -20,7 +20,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should be hidden for arrays.R
      *
      * @var array
      */
